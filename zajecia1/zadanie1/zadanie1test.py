b8ccf36677c820d97fefb277b7e040807606b654
Adam Kisiel <s441403@labs.wmi.amu.edu.pl>
2018-04-15 18:34:39 +0200

    correct scripts

--- zajecia1/zadanie1/zadanie1test.py
+++ zajecia1/zadanie1/zadanie1test.py
@@ -6,6 +6,7 @@ flats = pd.read_csv('train/train.tsv', sep = '\t',
         names = ['price', 'isNew', 'rooms', 'floor', 'location', 'sqrMeters'])
 
 infile = pd.read_csv('test-A/in.tsv', sep='\t')
+
 f = open('test-A/out.tsv', 'w')
 for i in range (0,len(infile)):
     f.write(str(flats.price.mean()) + '\n')
