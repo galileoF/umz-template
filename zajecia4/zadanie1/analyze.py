53e45f4d2c41a87968a24b686d092b53e287de32
Adam <adam.f.kisiel@gmail.com>
2018-06-06 20:35:49 +0200

    zadanie 1

--- zajecia4/zadanie1/analyze.py
+++ zajecia4/zadanie1/analyze.py
@@ -25,18 +25,86 @@ def show_plot():
     graph = graphviz.Source(dot_data)
     graph.view()
 
+# MAX LEAF NODES 6
+
+clf = tree.DecisionTreeClassifier(max_leaf_nodes=6)
+clf = clf.fit(train_X, train_Y)
+
+
+print('MAX LEAF NODES 6')
+print('TRAIN SET')
+#print(confusion_matrix(clf.predict(train_X), train_Y))
+print(sum(clf.predict(train_X) == train_Y) / len(train_X))
+print('TEST SET')
+#print(confusion_matrix(clf.predict(test_X), test_Y))
+print(sum(clf.predict(test_X) == test_Y) / len(test_X))
+
+# CRITERION 'entropy', MIN SAMPLES SPLIT 3
+
+clf = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split=3)
+clf = clf.fit(train_X, train_Y)
+
+
+print('CRITERION entropy, MIN SAMPLES SPLIT 3')
+print('TRAIN SET')
+#print(confusion_matrix(clf.predict(train_X), train_Y))
+print(sum(clf.predict(train_X) == train_Y) / len(train_X))
+print('TEST SET')
+#print(confusion_matrix(clf.predict(test_X), test_Y))
+print(sum(clf.predict(test_X) == test_Y) / len(test_X))
+
+
+# CRITERION 'entropy', MIN SAMPLES SPLIT 4
+
+clf = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split=4)
+clf = clf.fit(train_X, train_Y)
+
+
+print('CRITERION entropy, MIN SAMPLES SPLIT 4')
+print('TRAIN SET')
+#print(confusion_matrix(clf.predict(train_X), train_Y))
+print(sum(clf.predict(train_X) == train_Y) / len(train_X))
+print('TEST SET')
+#print(confusion_matrix(clf.predict(test_X), test_Y))
+print(sum(clf.predict(test_X) == test_Y) / len(test_X))
+
+# MIN WEIGHT FRACTION LEAF 0.1
+
+clf = tree.DecisionTreeClassifier(min_weight_fraction_leaf=0.1)
+clf = clf.fit(train_X, train_Y)
+
+
+print('MIN WEIGHT FRACTION LEAF 0.1')
+print('TRAIN SET')
+#print(confusion_matrix(clf.predict(train_X), train_Y))
+print(sum(clf.predict(train_X) == train_Y) / len(train_X))
+print('TEST SET')
+#print(confusion_matrix(clf.predict(test_X), test_Y))
+print(sum(clf.predict(test_X) == test_Y) / len(test_X))
+
+# MIN WEIGHT FRACTION LEAF 0.3
+clf = tree.DecisionTreeClassifier(min_weight_fraction_leaf=0.3)
+clf = clf.fit(train_X, train_Y)
+
+
+print('MIN WEIGHT FRACTION LEAF 0.3')
+print('TRAIN SET')
+#print(confusion_matrix(clf.predict(train_X), train_Y))
+print(sum(clf.predict(train_X) == train_Y) / len(train_X))
+print('TEST SET')
+#print(confusion_matrix(clf.predict(test_X), test_Y))
+print(sum(clf.predict(test_X) == test_Y) / len(test_X))
+
+# PRESORT True, MAX DEPTH 4
 
 clf = tree.DecisionTreeClassifier(presort=True, max_depth=4)
 clf = clf.fit(train_X, train_Y)
-show_plot()
+#show_plot()
 
-# clf = tree.DecisionTreeClassifier(max_depth=4)
-# clf = clf.fit(train_X, train_Y)
-# show_plot()
-
+print('PRESORT True, MAX DEPTH 4')
 print('TRAIN SET')
-print(confusion_matrix(clf.predict(train_X), train_Y))
+#print(confusion_matrix(clf.predict(train_X), train_Y))
 print(sum(clf.predict(train_X) == train_Y) / len(train_X))
 print('TEST SET')
-print(confusion_matrix(clf.predict(test_X), test_Y))
+#print(confusion_matrix(clf.predict(test_X), test_Y))
 print(sum(clf.predict(test_X) == test_Y) / len(test_X))
